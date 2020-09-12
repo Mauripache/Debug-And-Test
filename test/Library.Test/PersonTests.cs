c00cfa9ba0216bd578913341fae29c539d28c6da
Mauripache <mauri_pache@hotmail.com>
2020-09-12 00:42:56 -0300

    Final commit with tests

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -6,24 +6,54 @@ namespace Tests
 {
     public class Tests
     {
+        private Person person;
+
         [SetUp]
         public void Setup()
         {
-            // Insertá tu código de inicialización aquí
+            this.person = new Person("Mauricio", "5.377.044-0");
         }
 
         [Test]
-        public void PersonWithValidId()
+        public void AssignValidId()
         {
-            Person person = new Person("Test", "5.377.044-0");
-            string name = "Test";
-            string id = "5.377.044-0";
+            person.ID = "2.750.720-5";
+            Assert.AreEqual(person.ID, "2.750.720-5");
         }   
 
         [Test]
-        public void PersonWithWrongVerifierDigit()
+        public void AssignIdWrongVerifierDigit()
         {
-            Person person = new Person("Test", "5.377.044-0");
+            person.ID = "2.750.720-7";
+            Assert.AreEqual(person.ID, "5.377.044-0");
+        }
+
+        [Test]
+        public void AssignNameValid()
+        {
+            person.Name= "Santiago";
+            Assert.AreEqual(person.Name, "Santiago");
+        }
+
+        [Test]
+        public void AssignNameInvalid()
+        {
+            person.Name = "";
+            Assert.AreEqual(person.Name, "Mauricio");
+        }
+
+        [Test]
+        public void AssignIdWrongFormat()
+        {
+            person.ID = "5|377|044|0";
+            Assert.AreEqual(person.ID, "5.377.044-0");
+        }
+
+        [Test]
+        public void AssignIdWrongLength()
+        {
+            person.ID = "9108309182309182093810923";
+            Assert.AreEqual(person.ID, "5.377.044-0");
         }
     }
 }
